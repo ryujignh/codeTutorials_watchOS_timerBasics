ba2db2bad3c95eac2f6f53242357631f964c0b64
ryujignh <ryujignh@gmail.com>
2020-12-31 12:34:48 +0900

    Add timer. Main view and second view

--- codeTutorials_watchOS_timerBasics WatchKit Extension/ContentView.swift
+++ codeTutorials_watchOS_timerBasics WatchKit Extension/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var secondScreenShown = false
+    @State var timerVal = 1
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("Start timer for \(timerVal) seconds")
+                .font(.system(size: 14))
+            Picker(selection: $timerVal, label: Text("")) {
+                /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+            }
+            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal), isActive: $secondScreenShown, label: { Text("Go")})
+        }
+        
+    }
+}
+
+struct SecondView: View {
+    @Binding var secondScreenShown:Bool
+    @State var timerVal:Int
+    var body: some View {
+        if timerVal > 0 {
+    
+            VStack {
+                Text("Time remaining")
+                    .font(.system(size: 14))
+                Text("\(timerVal)")
+                    .font(.system(size: 40))
+                    .onAppear(){
+
+                        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                            if self.timerVal > 0 {
+                                self.timerVal -= 1
+                            }
+                        }
+                    }
+                Text("seconds")
+                    .font(.system(size: 14))
+                Button(action: {
+                    self.secondScreenShown = false
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                })
+            }
+        } else {
+            Button(action: {
+                self.secondScreenShown = false
+            }, label: {
+                Text("Done")
+                    .foregroundColor(.green)
+            })
+        }
+        
+        
     }
 }
 
